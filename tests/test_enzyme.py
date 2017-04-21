a2c5ee77d6418c4b8792fdc3cca9b0552999cb9c
Richard Hydomako <rhydomako@gmail.com>
2017-04-21 15:08:12 -0400

    Starting on tertinary structure calculation

--- tests/test_enzyme.py
+++ tests/test_enzyme.py
@@ -1,12 +1,12 @@
 from typogenetics.enzyme import Enzyme, InvalidEnzyme
-import typogenetics.amino_acid
+import typogenetics.amino_acid as aa
 
 from nose.tools import assert_raises_regexp
 
 class TestEnzyme:
 
     def test_constructor(self):
-        Enzyme([typogenetics.amino_acid.cut(), typogenetics.amino_acid.delete()])
+        Enzyme([aa.cut(), aa.delete()])
 
     def test_input_not_a_list(self):
         with assert_raises_regexp(InvalidEnzyme, 'Input must be a list'):
@@ -14,4 +14,7 @@ class TestEnzyme:
 
     def test_input_all_amino_acids(self):
         with assert_raises_regexp(InvalidEnzyme, 'Must all be of type AminoAcid'):
-            Enzyme([typogenetics.amino_acid.cut(), typogenetics.amino_acid.delete(), 4])
+            Enzyme([aa.cut(), aa.delete(), 4])
+
+    def test_preferred_binding(self):
+        e = Enzyme([aa.cut(), aa.delete()])
--- typogenetics/enzyme.py
+++ typogenetics/enzyme.py
@@ -1,6 +1,20 @@
-from .amino_acid import AminoAcid
+import typogenetics.amino_acid as aa
 from collections import deque
 
+
+BINDING_PREFERENCES = {
+    'r':'A',
+    'u':'C',
+    'd':'G',
+    'l':'T'
+}
+
+FOLDING_TRANSLATION = {
+               'cut':'s', 'delete':'s', 'swi':'r',
+    'mvr':'s', 'mvl':'s', 'cop':'r',    'off':'l',
+    'ina':'s', 'inc':'r', 'ing':'r',    'int':'l',
+    'rpy':'r', 'rpu':'l', 'lpy':'l',    'lpu':'l'
+}
 
 class InvalidEnzyme(Exception):
     pass
@@ -12,7 +26,7 @@ def _check_amino_acids(amino_acid_list):
         raise InvalidEnzyme('Input must be a list')
 
     for amino_acid in amino_acid_list:
-        if isinstance(amino_acid, AminoAcid) == False:
+        if isinstance(amino_acid, aa.AminoAcid) == False:
             raise InvalidEnzyme("Must all be of type AminoAcid")
     return
 
@@ -25,4 +39,5 @@ class Enzyme(object):
         self.preferred_binding = self.preferred_binding()
 
     def preferred_binding(self):
+        foldings = [FOLDING_TRANSLATION[amino_acid.__class__.__name__] for amino_acid in self.amino_acids]
         return None
