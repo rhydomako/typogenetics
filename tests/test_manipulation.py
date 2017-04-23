342eb143643a7e8506edffae0cc5b218ee51be54
Richard Hydomako <rhydomako@gmail.com>
2017-04-22 21:47:42 -0400

    Implemented swi, and added a bunch of tests

--- tests/test_manipulation.py
+++ tests/test_manipulation.py
@@ -39,3 +39,286 @@ class TestManipulation:
         strand_strs = sorted([strand.strand for strand in final_strands])
 
         assert(strand_strs == ['AT', 'CAAAGAGAATCCTCTTTG', 'CAAAGAGGA'])
+
+    def test_cop(self):
+        """
+                   v
+        Secondary: ....
+        Primary:   ACGT
+                   ^
+        Copy mode: False
+
+        cop
+                   v
+        Secondary: T...
+        Primary:   ACGT
+                   ^
+        Copy mode: True
+        ['ACGT', 'T']
+        """
+        s = Strand('ACGT')
+        e = Enzyme([aa.cop()])
+
+        final_strands = apply_enzyme(s, e)
+        strand_strs = sorted([strand.strand for strand in final_strands])
+        assert(strand_strs == ['ACGT', 'T'])
+
+    def test_cut(self):
+        """
+                   v
+        Secondary: ....
+        Primary:   ACGT
+                   ^
+        Copy mode: False
+        cut
+                   v
+        Secondary: .
+        Primary:   A
+                   ^
+        Copy mode: False
+        ['A', 'CGT']
+        """
+        s = Strand('ACGT')
+        e = Enzyme([aa.cut()])
+
+        final_strands = apply_enzyme(s, e)
+        strand_strs = sorted([strand.strand for strand in final_strands])
+        assert(strand_strs == ['A', 'CGT'])
+
+    def test_cop_mvr(self):
+        """
+                   v
+        Secondary: ....
+        Primary:   ACGT
+                   ^
+        Copy mode: False
+        cop
+                   v
+        Secondary: T...
+        Primary:   ACGT
+                   ^
+        Copy mode: True
+        mvr
+                    v
+        Secondary: TG..
+        Primary:   ACGT
+                    ^
+        Copy mode: True
+        ['ACGT', 'GT']
+        """
+        s = Strand('ACGT')
+        e = Enzyme([aa.cop(), aa.mvr()])
+
+        final_strands = apply_enzyme(s, e)
+        strand_strs = sorted([strand.strand for strand in final_strands])
+        assert(strand_strs == ['ACGT', 'GT'])
+
+    def test_cop_mvl(self):
+        """
+                     v
+        Secondary: ....
+        Primary:   ACGT
+                     ^
+        Copy mode: False
+        mvr
+                      v
+        Secondary: ....
+        Primary:   ACGT
+                      ^
+        Copy mode: False
+        cop
+                      v
+        Secondary: ...A
+        Primary:   ACGT
+                      ^
+        Copy mode: True
+        mvl
+                     v
+        Secondary: ..CA
+        Primary:   ACGT
+                     ^
+        Copy mode: True
+        ['AC', 'ACGT']
+        """
+        s = Strand('ACGT')
+        e = Enzyme([aa.mvr(), aa.cop(), aa.mvl()])
+
+        final_strands = apply_enzyme(s, e)
+        strand_strs = sorted([strand.strand for strand in final_strands])
+        assert(strand_strs == ['AC','ACGT'])
+
+    def test_ina(self):
+        """
+                   v
+        Secondary: ....
+        Primary:   ACGT
+                   ^
+        Copy mode: False
+        inc
+                   v
+        Secondary: .....
+        Primary:   ACCGT
+                   ^
+        Copy mode: False
+        ['ACCGT']
+        """
+        s = Strand('ACGT')
+        e = Enzyme([aa.ina()])
+
+        final_strands = apply_enzyme(s, e)
+        strand_strs = sorted([strand.strand for strand in final_strands])
+        assert(strand_strs == ['AACGT'])
+    
+    def test_inc_cop(self):
+        """
+                   v
+        Secondary: ....
+        Primary:   ACGT
+                   ^
+        Copy mode: False
+        cop
+                   v
+        Secondary: T...
+        Primary:   ACGT
+                   ^
+        Copy mode: True
+        inc
+                   v
+        Secondary: TG...
+        Primary:   ACCGT
+                   ^
+        Copy mode: True
+        ['ACCGT', 'GT']
+        """
+        s = Strand('ACGT')
+        e = Enzyme([aa.cop(), aa.inc()])
+
+        final_strands = apply_enzyme(s, e)
+        strand_strs = sorted([strand.strand for strand in final_strands])
+        assert(strand_strs == ['ACCGT', 'GT'])
+
+    def test_ing_cop(self):
+        """
+                   v
+        Secondary: ....
+        Primary:   ACGT
+                   ^
+        Copy mode: False
+        cop
+           v
+        Secondary: T...
+        Primary:   ACGT
+                   ^
+        Copy mode: True
+        off
+                   v
+        Secondary: T...
+        Primary:   ACGT
+                   ^
+        Copy mode: False
+        mvr
+                    v
+        Secondary: T...
+        Primary:   ACGT
+                    ^
+        Copy mode: False
+        mvr
+                     v
+        Secondary: T...
+        Primary:   ACGT
+                     ^
+        Copy mode: False
+        cop
+                     v
+        Secondary: T.C.
+        Primary:   ACGT
+                     ^
+        Copy mode: True
+        ing
+                     v
+        Secondary: T.CC.
+        Primary:   ACGGT
+                     ^
+        Copy mode: True
+        ['ACGGT', 'CC', 'T']
+        """
+        s = Strand('ACGT')
+        e = Enzyme([aa.cop(), aa.off(), aa.mvr(), aa.mvr(), aa.cop(), aa.ing()])
+
+        final_strands = apply_enzyme(s, e)
+        strand_strs = sorted([strand.strand for strand in final_strands])
+        assert(strand_strs == ['ACGGT', 'CC', 'T'])
+
+    def test_swi(self):
+        """
+                   v
+        Secondary: ....
+        Primary:   ACGT
+                   ^
+        Copy mode: False
+        cop
+                   v
+        Secondary: T...
+        Primary:   ACGT
+                   ^
+        Copy mode: True
+        swi
+                      v
+        Secondary: TGCA
+        Primary:   ...T
+                      ^
+        Copy mode: True
+        ['ACGT', 'T']
+        """
+        s = Strand('ACGT')
+        e = Enzyme([aa.cop(), aa.swi()])
+
+        final_strands = apply_enzyme(s, e)
+        strand_strs = sorted([strand.strand for strand in final_strands])
+        assert(strand_strs == ['ACGT', 'T'])
+    
+    def test_swi_swi(self):
+        """
+                     v
+        Secondary: ....
+        Primary:   ACGT
+                     ^
+        Copy mode: False
+        cop
+                     v
+        Secondary: ..C.
+        Primary:   ACGT
+                     ^
+        Copy mode: True
+        swi
+                    v
+        Secondary: TGCA
+        Primary:   .C..
+                    ^
+        Copy mode: True
+        swi
+                     v
+        Secondary: ..C.
+        Primary:   ACGT
+                     ^
+        """
+        s = Strand('ACGT')
+        e = Enzyme([aa.cop(), aa.swi(), aa.swi()])
+
+        final_strands = apply_enzyme(s, e)
+        strand_strs = sorted([strand.strand for strand in final_strands])
+        assert(strand_strs == ['ACGT', 'C'])
+    
+    def test_swi_none(self):
+        """
+                   v
+        Secondary: ....
+        Primary:   ACGT
+                   ^
+        """
+        s = Strand('ACGT')
+        e = Enzyme([aa.swi()])
+
+        final_strands = apply_enzyme(s, e)
+        strand_strs = sorted([strand.strand for strand in final_strands])
+        assert(strand_strs == ['ACGT'])
--- typogenetics/manipulation.py
+++ typogenetics/manipulation.py
@@ -8,11 +8,7 @@ BASE_COMPLEMENT = {
     'G':'C', 'C':'G'
 }
 
-class OutOfBoundsRight(Exception):
-    pass
-
-
-class OutOfBoundsLeft(Exception):
+class OutOfStrandException(Exception):
     pass
 
 
@@ -42,40 +38,82 @@ class StrandManipulationBuffer(object):
         self.primary.right.clear()
         self.secondary.right.clear()
 
-    def mvr(self):
-        if self.copy_mode:
-            self.secondary.bound = BASE_COMPLEMENT[self.primary.bound]
-        if (self.primary.bound):
-            self.primary.left.append(self.primary.bound)
-            self.secondary.left.append(self.secondary.bound)
-        if len(self.primary.right):
-            self.primary.bound = self.primary.right.popleft()
-            self.secondary.bound = self.secondary.right.popleft()
-        else:
-            raise OutOfBoundsRight
-
-    def mvl(self):
-        if self.copy_mode:
-            self.secondary.bound = BASE_COMPLEMENT[self.primary.bound]
-        if self.primary.bound:
-            self.primary.right.appendleft(self.primary.bound)
-            self.secondary.right.appendleft(self.secondary.bound)
-        if len(self.primary.left):
-            self.primary.bound = self.primary.left.pop()
-            self.secondary.bound = self.secondary.left.pop()
-        else:
-            raise OutOfBoundsLeft
+    def swi(self):
+        if not self.secondary.bound:
+            raise OutOfStrandException
+        #swap bound
+        tmp = self.primary.bound
+        self.primary.bound = self.secondary.bound
+        self.secondary.bound = tmp
+        #swap right-left
+        self.primary.right.reverse()
+        self.secondary.left.reverse()
+        tmp = self.primary.right
+        self.primary.right = self.secondary.left
+        self.secondary.left = tmp
+        #swap left-right
+        self.primary.left.reverse()
+        self.secondary.right.reverse()
+        tmp = self.primary.left
+        self.primary.left = self.secondary.right
+        self.secondary.right = tmp
 
     def delete(self):
         self.primary.bound = None
         self.secondary.bound = None
         self.mvr()
 
+    def mvr(self):
+        if (self.primary.bound):
+            self.primary.left.append(self.primary.bound)
+            self.secondary.left.append(self.secondary.bound)
+        if len(self.primary.right):
+            self.primary.bound = self.primary.right.popleft()
+            self.secondary.bound = self.secondary.right.popleft()
+            if self.copy_mode:
+                self.secondary.bound = BASE_COMPLEMENT[self.primary.bound]
+        else:
+            raise OutOfStrandException
+
+    def mvl(self):
+        if self.primary.bound:
+            self.primary.right.appendleft(self.primary.bound)
+            self.secondary.right.appendleft(self.secondary.bound)
+        if len(self.primary.left):
+            self.primary.bound = self.primary.left.pop()
+            self.secondary.bound = self.secondary.left.pop()
+            if self.copy_mode:
+                self.secondary.bound = BASE_COMPLEMENT[self.primary.bound]
+        else:
+            raise OutOfStrandException
+
     def cop(self):
+        self.secondary.bound = BASE_COMPLEMENT[self.primary.bound]
         self.copy_mode = True
 
     def off(self):
         self.copy_mode = False
+
+    def ina(self):
+        self.primary.right.appendleft('A')
+        if self.copy_mode:
+            self.secondary.right.appendleft(BASE_COMPLEMENT['A'])
+        else:
+            self.secondary.right.appendleft(None)
+
+    def inc(self):
+        self.primary.right.appendleft('C')
+        if self.copy_mode:
+            self.secondary.right.appendleft(BASE_COMPLEMENT['C'])
+        else:
+            self.secondary.right.appendleft(None)
+
+    def ing(self):
+        self.primary.right.appendleft('G')
+        if self.copy_mode:
+            self.secondary.right.appendleft(BASE_COMPLEMENT['G'])
+        else:
+            self.secondary.right.appendleft(None)
 
     def int(self):
         self.primary.right.appendleft('T')
@@ -137,37 +175,50 @@ def apply_enzyme(strand, enzyme, verbose=False):
     primary_strands = []
     secondary_strands = []
 
-    # TODO::swi
     for amino_acid in enzyme.amino_acids:
-        if amino_acid.__class__.__name__ == 'cut':
-            primary_strands.append( ''.join([(c or '.') for c in sm.primary.right]) )
-            secondary_strands.append( ''.join([(c or '.') for c in sm.secondary.right]) )
-            sm.cut()
-        elif amino_acid.__class__.__name__ == 'delete':
-            sm.delete()
-        elif amino_acid.__class__.__name__ == 'mvr':
-            sm.mvr()
-        elif amino_acid.__class__.__name__ == 'mvl':
-            sm.mvl()
-        elif amino_acid.__class__.__name__ == 'cop':
-            sm.cop()
-        elif amino_acid.__class__.__name__ == 'off':
-            sm.off()
-        elif amino_acid.__class__.__name__ == 'int':
-            sm.int()
-        elif amino_acid.__class__.__name__ == 'rpu':
-            sm.rpu()
-        elif amino_acid.__class__.__name__ == 'rpy':
-            sm.rpy()
+        try:
+            if amino_acid.__class__.__name__ == 'cut':
+                primary_strands.append( ''.join([(c or '.') for c in sm.primary.right]) )
+                secondary_strands.append( ''.join([(c or '.') for c in sm.secondary.right]) )
+                sm.cut()
+            elif amino_acid.__class__.__name__ == 'swi':
+                sm.swi()
+            elif amino_acid.__class__.__name__ == 'delete':
+                sm.delete()
+            elif amino_acid.__class__.__name__ == 'mvr':
+                sm.mvr()
+            elif amino_acid.__class__.__name__ == 'mvl':
+                sm.mvl()
+            elif amino_acid.__class__.__name__ == 'cop':
+                sm.cop()
+            elif amino_acid.__class__.__name__ == 'off':
+                sm.off()
+            elif amino_acid.__class__.__name__ == 'ina':
+                sm.ina()
+            elif amino_acid.__class__.__name__ == 'inc':
+                sm.inc()
+            elif amino_acid.__class__.__name__ == 'ing':
+                sm.ing()
+            elif amino_acid.__class__.__name__ == 'int':
+                sm.int()
+            elif amino_acid.__class__.__name__ == 'rpu':
+                sm.rpu()
+            elif amino_acid.__class__.__name__ == 'rpy':
+                sm.rpy()
 
-        if verbose:
-            print amino_acid.__class__.__name__
-            print sm
+            if verbose:
+                print amino_acid.__class__.__name__
+                print sm
+        except OutOfStrandException:
+            break
 
     primary_strands.append(sm.primary.dump())
     secondary_strands.append(sm.secondary.dump())
 
-    strands.extend(primary_strands)
+    for strand in primary_strands:
+        for sub_strand in strand.split('.'):
+            strands.append(sub_strand)
+
     for strand in secondary_strands:
         for sub_strand in strand.split('.'):
             strands.append(sub_strand[::-1])
